6f02554204643f3fd1d5b86a2277003b88b26cb8
nafis-tech <nafiezlela9@gmail.com>
2021-11-05 16:37:45 +0700

    samb

---
+++ scripts.js
@@ -0,0 +1,23 @@
+let users = [
+    {
+        nama : 'nina',
+        noTelp : 12345,
+        noWa : 6789,
+        posisi : 'web developer'
+    },
+    {
+        nama : 'nuna',
+        noTelp : 12345,
+        noWa : 6789,
+        posisi : 'web developer'
+    },
+]
+
+class Pelamar {
+    constructor(nama, noTelp, noWa, posisi){
+        this.nama = nama
+        this.noTelp = noTelp
+        this.noWa = noWa
+        this.posisi = posisi
+    }
+}
